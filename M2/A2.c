2a28ac4676d9da58c75b1e9b5d023b2922294b89
priyankamaladi77 <svanmp@msitprogram.net>
2019-05-12 22:55:43 +0530

    A2

---
+++ M2/A2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int hexa(char str[]);
+int main(void) {
+    char str[1024];
+    int i, c;
+    for (i=0; (c = getchar()) != '\n'; i++)
+        str[i] = c;
+    str[i] = '\0';
+    printf("%s: %d\n", str, hexa(str));
+    return EXIT_SUCCESS;
+}
+
+int hexa(char str[]) {
+    int i;
+    int c, num;
+    num = 0;
+    for (i = 0; (c = str[i]) != '\0'; ++i) {
+        num *= 16;
+        if (i == 0 && c == '0') {
+            c = str[++i];
+            if (c != 'x' && c != 'X')
+                --i;
+        } else if (c >= '0' && c <= '9')
+            num += c - '0';
+        else if (c >= 'a' && c <= 'f')
+            num += 10 + (c - 'a');
+        else if (c >= 'A' && c <= 'F')
+            num += 10 + (c - 'A');
+        else
+            return num;
+    }
+    return num;
+}
